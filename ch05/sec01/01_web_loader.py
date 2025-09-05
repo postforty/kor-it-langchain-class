1118686533fad9f3e575e29e69ebfbb6951f4580
postforty <postforty@naver.com>
2025-09-05 09:38:58 +0900

    docs: README.md에 챗봇 개발 과정 추가 및 관련 파일 삭제

--- ch05/sec01/01_web_loader.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-loader = WebBaseLoader('https://www.langchain.com/')
-docs = loader.load()
-
-print(docs)
--- ch05/sec01/02_pdf_loader.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader('../data/KCI_FI003153549.pdf')
-pages = loader.load()
-
-print(pages)
